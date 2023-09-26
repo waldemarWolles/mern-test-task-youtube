7991c8674bc8486e93229138bca90b7df3ba5a83
Waldemar <chooter1999@gmail.com>
2023-09-26 20:25:38 +0300

    finished project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css"
+import { Home } from "./pages"
 
-function App() {
-  return <>Client code</>
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  )
 }
 
 export default App
